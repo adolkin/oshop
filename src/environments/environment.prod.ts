d03d4d5371ac809040ebe91ea952f8591deee12b
mradol <tuanbk189@gmail.com>
2017-09-15 10:49:14 +1000

    Display products homepage and category, admin components

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCnVr5uHxBSUh-RRLqS25Zz5paotXVV6V4",
+    authDomain: "oshop-bf07b.firebaseapp.com",
+    databaseURL: "https://oshop-bf07b.firebaseio.com",
+    projectId: "oshop-bf07b",
+    storageBucket: "oshop-bf07b.appspot.com",
+    messagingSenderId: "38604706572"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCnVr5uHxBSUh-RRLqS25Zz5paotXVV6V4",
+    authDomain: "oshop-bf07b.firebaseapp.com",
+    databaseURL: "https://oshop-bf07b.firebaseio.com",
+    projectId: "oshop-bf07b",
+    storageBucket: "oshop-bf07b.appspot.com",
+    messagingSenderId: "38604706572"
+  }
+};
