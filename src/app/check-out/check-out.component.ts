cffd4ed2bbfc82d65c649098892e99e40ed15d9e
mradol <tuanbk189@gmail.com>
2017-09-24 22:02:25 +1000

    Saving the Order in Firebase

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,14 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ShoppingCart } from 'app/models/shopping-cart';
+import { Subscription } from 'rxjs/Subscription';
+import { ShoppingCartService } from 'app/shopping-cart.service';
+import { OrderService } from 'app/order.service';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent { 
+export class CheckOutComponent implements OnDestroy { 
   shipping = {}; 
-  
+  cart: ShoppingCart;
+  subscription: Subscription;
+
+  constructor(
+    private orderService: OrderService,
+    private shoppingCartService: ShoppingCartService) {}
+
+  async ngOnInit() {
+    let cart$ = await this.shoppingCartService.getCart();
+    cart$.subscribe(cart => this.cart = cart);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
   placeOrder() {
-    console.log(this.shipping);
+    let order = {
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map( i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrll: i.imageUrl,
+            price: i.price
+          },
+          quanity: i.quantity,
+          totalPrice: i.totalPrice
+        }
+      })
+    };
+
+    this.orderService.storeOrder(order);
   }    
 }
