e4abe8e8a1a8eac805cce1201f6132bfe14a26df
mradol <tuanbk189@gmail.com>
2017-09-28 02:17:10 +1000

    Importing and Exporting Modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,6 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'shared/shared.module';
 
 import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
 import { HomeComponent } from './components/home/home.component';
@@ -8,7 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   imports: [
-    CommonModule,
+    SharedModule,
     RouterModule.forChild([])
   ],
   declarations: [    
